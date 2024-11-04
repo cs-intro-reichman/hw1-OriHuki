3853ab4b82b55d33eb0468150c2e783c09362faa
Ori Hukima <ori6hukima@gmail.com>
2024-11-04 21:42:59 +0200

    second version of Bill3

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,6 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-    // checking if the user insert 4 args 
-    if (args.length==4) { 
     //Insert 4 args, 3 String, 1 Int. 
 		String name1 = args[0];
         String name2 = args[1]; 
@@ -11,8 +9,6 @@ public class Bill3 {
     //Calculate the avg price for each person and rounding it up.     
         double eachPrice= value/3.0;
         double newPrice= Math.ceil(eachPrice);
-        System.out.println("Pay " + newPrice +" Shekels each.");}
-		else if (args.length !=4) {
-			System.out.println("You didnt insert args as requested"); }    
+        System.out.println("Pay " + newPrice +" Shekels each.");
        }
 	}
