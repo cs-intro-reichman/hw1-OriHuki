a7fc12bad12841c375073e2bbf737649d1b2ea8d
Ori Hukima <ori6hukima@gmail.com>
2024-11-05 15:47:24 +0200

    'Version2'

--- Bill3.java
+++ Bill3.java
@@ -5,10 +5,10 @@ public class Bill3 {
 		String name1 = args[0];
         String name2 = args[1]; 
         String name3 = args[2]; 
-        int value = Integer.parseInt(args[3]);
+        int value = Integer.parseInt(arggit pushs[3]);
     //Calculate the avg price for each person and rounding it up.     
         double eachPrice= value/3.0;
         double newPrice= Math.ceil(eachPrice);
-        System.out.println("Dear" + name1 + name2 + name3 + "Pay " + newPrice +" Shekels each.");
+        System.out.println("Dear" + name1 + ", " + name2 + " and " + name3 + " Pay " + newPrice +" Shekels each.");
        }
 	}
